b4d8d7a1113edadf7914303123751134edbd7dda
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-19 10:59:10 +0530

    Update house_pred_deployment.py

--- deployment_files/house_pred_deployment.py
+++ deployment_files/house_pred_deployment.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
-import xgboost
+import pickle
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
